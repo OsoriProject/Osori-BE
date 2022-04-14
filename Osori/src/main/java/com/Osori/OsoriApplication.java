cf6423ed662ca8a9a7dd8df77a9d6d39a0b468b1
minseok <kms9887@naver.com>
2022-04-14 14:37:49 +0900

    chore : Create new Project 프로젝트 생성 gitignore 파일 생성

---
+++ Osori/src/main/java/com/Osori/OsoriApplication.java
@@ -0,0 +1,13 @@
+package com.Osori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsoriApplication.class, args);
+	}
+
+}

a1e5588d70cd3dbeb2fa49b8ba41d40b026e01bf
minseok <kms9887@naver.com>
2022-04-15 11:27:45 +0900

    feat : Add files for using jpa about user user 엔티티 추가 user 레포지토리 추가

---
+++ Osori/src/main/java/com/Osori/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Osori.domain.repository;
+
+import com.Osori.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

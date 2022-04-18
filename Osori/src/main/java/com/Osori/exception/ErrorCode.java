a13d9897c4772471574c236398bec95594f4a0a2
minseok <kms9887@naver.com>
2022-04-18 12:58:27 +0900

    feat : Add files for error handling 에러 처리를 위한 로직 구현 - ExecptionController   - 예외에 따른 에러 처리 요청 - ErrorCode   - 에러의 상태를 가지고 있는 enum 객체 - ErrorResponse   - 에러를 반환하기 위한 객체 - CustomException   - 커스텀 예외 처리용 클래스

---
+++ Osori/src/main/java/com/Osori/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.Osori.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."), // 400
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드 입니다."), //405
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류 입니다."); // 500
+
+    private final HttpStatus status;
+    private final String message;
+}

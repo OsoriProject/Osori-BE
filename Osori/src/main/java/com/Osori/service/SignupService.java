1338e3bd2616d2b137f8eddf88e553e7e6eba134
minseok <kms9887@naver.com>
2022-04-18 12:59:37 +0900

    feat : Error handling in signup 회원 가입 시 에러 처리 - 중복된 이메일 존재하는 경우 에러

--- Osori/src/main/java/com/Osori/service/SignupService.java
+++ Osori/src/main/java/com/Osori/service/SignupService.java
@@ -3,6 +3,8 @@ package com.Osori.service;
 import com.Osori.domain.entity.User;
 import com.Osori.domain.repository.UserRepository;
 import com.Osori.dto.UserDto;
+import com.Osori.exception.CustomException;
+import com.Osori.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,10 @@ public class SignupService {
     private final UserRepository userRepository;
 
     public void Register(UserDto userDto){
+        if(userRepository.existsByEmail(userDto.getEmail())){
+            throw new CustomException(ErrorCode.ALREADY_EXIST_EMAIL);
+        }
+
         User user = User.builder()
                 .email(userDto.getEmail())
                 .password(userDto.getPassword())
